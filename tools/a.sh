fe2ed6b4c5ca6fdaaa163bb8f256c7fa69e43297
Alejandro R <73983571+Bl4ckTrlck@users.noreply.github.com>
2022-02-22 19:07:08 -0600

    2.0

--- tools/a.sh
+++ tools/a.sh
@@ -1,60 +1,95 @@
 export CURRENTUSER=$(whoami)
 SCRIPTDIR=$(readlink -f "$0")
 CURRENTDIR=$(dirname "$SCRIPTDIR")
-TOOLS=$CURRENTDIR/tools
-A=$CURRENTDIR/system/
-B=$CURRENTDIR/vendor/
-C=$CURRENTDIR/system_ext/
-D=$CURRENTDIR/product/
-E=$CURRENTDIR/odm/
+TOOLS=$CURRENTDIR
+IMG=$CURRENTDIR/images
+A=$CURRENTDIR/images/system
+B=$CURRENTDIR/images/vendor
+C=$CURRENTDIR/images/system_ext
+D=$CURRENTDIR/images/product
+E=$CURRENTDIR/images/odm
 ROM=$(readlink -f "$1")
-if [[ -z $ROM ]] 
-then
-echo "usage: ./a.sh rom.zip" && exit
-fi
+
 set -e
 
 #MI
-sudo chmod +x $TOOLS/*
-sudo rm -rf $A $B $C $D $E || true
-mkdir $A $B $C $D $E || true
+rm -r $IMG || true
+mkdir $IMG $A $B $C $D $E 
 printf "\n" 
 printf "\n" 
-printf "extracting zip files..."
+printf "convertingzip files..."
 printf "\n" 
 printf "\n" 
-
-#Extract zip
-unzip -d $A $ROM system.transfer.list system.new.dat.br || true
+sleep 1s
+#SYSTEM
+if unzip -d $A $ROM system.transfer.list system.new.dat.br
+then
+brotli -j -v -d $A/system.new.dat.br -o $A/system.new.dat 
+$TOOLS/sdat2img/sdat2img.py $A/system.transfer.list $A/system.new.dat $A/system.img 
 printf "\n" 
-unzip -d $B $ROM vendor.transfer.list vendor.new.dat.br || true
 printf "\n" 
-unzip -d $C $ROM system_ext.transfer.list system_ext.new.dat.br || true
+printf "converting system..."
 printf "\n" 
-unzip -d $D $ROM product.transfer.list product.new.dat.br || true
+rm -r $A/system.new.dat $A/system.transfer.list
+else
+echo "skipping system - not founded" 
+sleep 1s
+fi
+#VENDOR
+if unzip -d $B $ROM vendor.transfer.list vendor.new.dat.br 
+then
 printf "\n" 
-unzip -d $E $ROM odm.transfer.list odm.new.dat.br || true
 printf "\n" 
-
-#IMGS
-brotli -j -v -d $A/system.new.dat.br -o $A/system.new.dat || true
-$TOOLS/sdat2img.py $A/system.transfer.list $A/system.new.dat $A/system.img || true
+brotli -j -v -d $B/vendor.new.dat.br -o $B/vendor.new.dat 
+$TOOLS/sdat2img/sdat2img.py $B/vendor.transfer.list $B/vendor.new.dat $B/vendor.img 
+printf "converting vendor..."
+rm -r $B/vendor.new.dat $B/vendor.transfer.list
+else
+echo "skipping vendor - not founded" 
+sleep 1s
+fi
+#SYSTEM_EXT
+if unzip -d $C $ROM system_ext.transfer.list system_ext.new.dat.br 
+then
 printf "\n" 
-brotli -j -v -d $B/vendor.new.dat.br -o $B/vendor.new.dat || true
-$TOOLS/sdat2img.py $B/vendor.transfer.list $B/vendor.new.dat $B/vendor.img || true
 printf "\n"
-brotli -j -v -d $C/system_ext.new.dat.br -o $C/system_ext.new.dat || true
-$TOOLS/sdat2img.py $C/system_ext.transfer.list $C/system_ext.new.dat $C/system_ext.img || true
+brotli -j -v -d $C/system_ext.new.dat.br -o $C/system_ext.new.dat 
+$TOOLS/sdat2img/sdat2img.py $C/system_ext.transfer.list $C/system_ext.new.dat $C/system_ext.img 
+printf "converting system_ext..."
+rm -r $C/system_ext.new.dat $C/system_ext.transfer.list
+else
+echo "skipping system_ext - not founded" 
+sleep 1s
+fi
+#PRODUCT
+if unzip -d $D $ROM product.transfer.list product.new.dat.br 
+then
+printf "\n" 
 printf "\n"
-brotli -j -v -d $D/product.new.dat.br -o $D/product.new.dat || true
-$TOOLS/sdat2img.py $D/product.transfer.list $D/product.new.dat $D/product.img || true
+brotli -j -v -d $D/product.new.dat.br -o $D/product.new.dat 
+$TOOLS/sdat2img/sdat2img.py $D/product.transfer.list $D/product.new.dat $D/product.img 
+printf "converting product..."
+rm -r $D/product.new.dat $D/product.transfer.list
+else
+echo "skipping product - not founded"
+sleep 1s 
+fi
+#ODM
+if unzip -d $E $ROM odm.transfer.list odm.new.dat.br 
+then
+printf "\n" 
 printf "\n"
-brotli -j -v -d $E/odm.new.dat.br -o $E/odm.new.dat || true
-$TOOLS/sdat2img.py $E/odm.transfer.list $E/odm.new.dat $E/odm.img || true
+brotli -j -v -d $E/odm.new.dat.br -o $E/odm.new.dat 
+$TOOLS/sdat2img/sdat2img.py $E/odm.transfer.list $E/odm.new.dat $E/odm.img 
 printf "\n"
-
-rm -r $A/system.new.dat $A/system.transfer.list $B/vendor.new.dat $B/vendor.transfer.list $C/system_ext.new.dat $C/system_ext.transfer.list $D/product.new.dat $D/product.transfer.list $E/odm.new.dat $E/odm.transfer.list  || true
-	
+printf "convertin godm..."
+rm -r $E/odm.new.dat $E/odm.transfer.list 
+else
+echo "skipping odm - not founded" 
+sleep 1s
+fi	
+cd $CURRENTDIR 
+mv $IMG ..	
 printf "process finished"
 printf "\n" 
 printf "go to designed folder and you can close this terminal now..."
